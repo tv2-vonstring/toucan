a97805feaba3573301d3b3f727c112f340467c9f
Kristian von Streng Hæhre <kristian.hahre@tv2.no>
2026-04-23 12:11:42 +0200

    Honor toucan_FFmpeg in main build and export toucanResource
    Add toucan_FFmpeg option to the top-level build so Package.cmake only
    bundles FFmpeg shared libs when FFmpeg was actually built. Install
    toucanResource into the toucanRenderTargets export set so static
    toucanRender builds don't fail install(EXPORT) with a missing-target
    error.

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -3,18 +3,20 @@
 
 if(WIN32)
     set(CPACK_GENERATOR ZIP)
-    
-    set(INSTALL_DLLS
-        ${CMAKE_INSTALL_PREFIX}/bin/avcodec-62.dll
-        ${CMAKE_INSTALL_PREFIX}/bin/avdevice-62.dll
-        ${CMAKE_INSTALL_PREFIX}/bin/avformat-62.dll
-        ${CMAKE_INSTALL_PREFIX}/bin/avutil-60.dll
-        ${CMAKE_INSTALL_PREFIX}/bin/swresample-6.dll
-        ${CMAKE_INSTALL_PREFIX}/bin/swscale-9.dll)
-    
-    install(
-        FILES ${INSTALL_DLLS}
-        DESTINATION bin)
+
+    if(toucan_FFmpeg)
+        set(INSTALL_DLLS
+            ${CMAKE_INSTALL_PREFIX}/bin/avcodec-62.dll
+            ${CMAKE_INSTALL_PREFIX}/bin/avdevice-62.dll
+            ${CMAKE_INSTALL_PREFIX}/bin/avformat-62.dll
+            ${CMAKE_INSTALL_PREFIX}/bin/avutil-60.dll
+            ${CMAKE_INSTALL_PREFIX}/bin/swresample-6.dll
+            ${CMAKE_INSTALL_PREFIX}/bin/swscale-9.dll)
+
+        install(
+            FILES ${INSTALL_DLLS}
+            DESTINATION bin)
+    endif()
 
 elseif(APPLE)
 
@@ -24,56 +26,60 @@ elseif(APPLE)
         "@executable_path/../lib")
     #set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
-    set(INSTALL_DYLIBS
-        ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.62.11.100.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.62.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.62.1.100.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.62.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavformat.62.3.100.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavformat.62.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavformat.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavutil.60.8.100.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavutil.60.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libavutil.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libswresample.6.1.100.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libswresample.6.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libswresample.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libswscale.9.1.100.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libswscale.9.dylib
-        ${CMAKE_INSTALL_PREFIX}/lib/libswscale.dylib)
-        
-    install(
-        FILES ${INSTALL_DYLIBS}
-        DESTINATION lib)
+    if(toucan_FFmpeg)
+        set(INSTALL_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.62.11.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.62.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.62.1.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.62.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.62.3.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.62.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.60.8.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.60.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.6.1.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.6.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.9.1.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.9.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.dylib)
+
+        install(
+            FILES ${INSTALL_DYLIBS}
+            DESTINATION lib)
+    endif()
 
 else()
 
     set(CPACK_GENERATOR TGZ)
 
-    set(INSTALL_LIBS
-        ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.so
-        ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.so.62
-        ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.so.62.11.100
-        ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.so
-        ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.so.62
-        ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.so.62.1.100
-        ${CMAKE_INSTALL_PREFIX}/lib/libavformat.so
-        ${CMAKE_INSTALL_PREFIX}/lib/libavformat.so.62
-        ${CMAKE_INSTALL_PREFIX}/lib/libavformat.so.62.3.100
-        ${CMAKE_INSTALL_PREFIX}/lib/libavutil.so
-        ${CMAKE_INSTALL_PREFIX}/lib/libavutil.so.60
-        ${CMAKE_INSTALL_PREFIX}/lib/libavutil.so.60.8.100
-        ${CMAKE_INSTALL_PREFIX}/lib/libswresample.so
-        ${CMAKE_INSTALL_PREFIX}/lib/libswresample.so.6
-        ${CMAKE_INSTALL_PREFIX}/lib/libswresample.so.6.1.100
-        ${CMAKE_INSTALL_PREFIX}/lib/libswscale.so
-        ${CMAKE_INSTALL_PREFIX}/lib/libswscale.so.9
-        ${CMAKE_INSTALL_PREFIX}/lib/libswscale.so.9.1.100)
-    
-    install(
-        FILES ${INSTALL_LIBS}
-        DESTINATION lib)
+    if(toucan_FFmpeg)
+        set(INSTALL_LIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.so
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.so.62
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.so.62.11.100
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.so
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.so.62
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.so.62.1.100
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.so
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.so.62
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.so.62.3.100
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.so
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.so.60
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.so.60.8.100
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.so
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.so.6
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.so.6.1.100
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.so
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.so.9
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.so.9.1.100)
+
+        install(
+            FILES ${INSTALL_LIBS}
+            DESTINATION lib)
+    endif()
 
 endif()
